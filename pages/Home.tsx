56a2d2d71810ce4e40f486e9c28c8900848bb3d0
Suhani Pakhrin Tamang <suhaniyh@gmail.com>
2026-09-09 22:21:14 +0545

    feat/Home-Created Home structure by adding logo and designs.

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,10 +1,21 @@
-function Home(){
-    return (
-        <div>
-            <h1>Photo Booth</h1>
-            <button>Select</button>
-        </div>
-    );
+import "../Home.css";
+import logo from "../assets/logo.png";
+import booth from "../assets/booth.png";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="logo-block">
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+
+      <div className="booth-container">
+        <img src={booth} alt="Su-Hani Booth" className="booth" />
+      </div>
+
+      <button className="select-button">Select</button>
+    </div>
+  );
 }
 
 export default Home;
